5262d11f792a969c2d942185f414256e3d9cffc2
rabra <rabira.hierpa@gmail.com>
2022-09-23 07:50:03 +0300

    added github secrets

--- src/firebase.ts
+++ src/firebase.ts
@@ -9,10 +9,10 @@ import { getFirestore } from "firebase/firestore";
 // fire-maps web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: "729299773969",
   appId: "1:729299773969:web:ae7ff710fadfa7ef189fb0",
   measurementId: "G-Z0H60FETF3",
